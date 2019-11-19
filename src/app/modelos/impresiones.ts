cec31fb95036a8d42dffaa484c7d1b7a7f961346
victor jimenez <victor@adpos.pe>
2019-11-19 10:50:15 -0500

    .

---
+++ src/app/modelos/impresiones.ts
@@ -0,0 +1,7 @@
+export class Impresiones {
+    columnad_exchange_estimated_revenue: number;
+    columnad_exchange_impressions: number;
+    dimensionad_exchange_creative_sizes: string;
+    dimensionad_exchange_date: string;
+    dimensionad_exchange_device_category:string;
+}
